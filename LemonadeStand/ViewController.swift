f3350c4a18a313cb5a6a7302755e66d9d35ea474
Khang Huynh <khang.huynh@gmail.com>
2014-10-14 19:44:06 -0400

    Created storyboard and added outlets and actions

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -9,6 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Constants
+    
+    let lemonPrice = 2.00
+    let icePrice = 1.00
+    
+    // Stats
+    
+    var currentCash = 10.00
+    var currentLemons = 1
+    var currentIce = 1
+    
+    // Labels
+    
+    @IBOutlet weak var currentCashLabel: UILabel!
+    @IBOutlet weak var currentLemonsLabel: UILabel!
+    @IBOutlet weak var currentIceLabel: UILabel!
+    
+    @IBOutlet weak var lemonsForPurchaseLabel: UILabel!
+    @IBOutlet weak var iceForPurchaseLabel: UILabel!
+    
+    
+    @IBOutlet weak var lemonsToMixLabel: UILabel!
+    
+    @IBOutlet weak var iceToMixLabel: UILabel!
+    
+    // Buttons
+    
+    @IBAction func purchaseLemonPressed(sender: AnyObject) {
+    }
+
+    @IBAction func unpurchaseLemonPressed(sender: AnyObject) {
+    }
+
+    @IBAction func purchaseIcePressed(sender: AnyObject) {
+    }
+
+    @IBAction func unpurchaseIcePressed(sender: AnyObject) {
+    }
+    
+    @IBAction func addLemonMixPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func subtractLemonMixPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func addIceMixPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func subtractIceMixPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func startButtonPressed(sender: AnyObject) {
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
